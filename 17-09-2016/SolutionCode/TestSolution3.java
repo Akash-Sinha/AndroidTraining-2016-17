755bcd30e09c5667140101ccc0df69195ae04bc8
Akash-Sinha <akashsinha.369@gmail.com>
2016-09-17 13:18:43 +0530

    Update TestSolution3.java

--- 17-09-2016/SolutionCode/TestSolution3.java
+++ 17-09-2016/SolutionCode/TestSolution3.java
@@ -3,10 +3,10 @@ public class TestSolution3 {
 	public static void main(String args[]) {
 		Solution3 s3 = new Solution3();
 		
-		System.out.println(s3.toString());
-		/*if(s3.containE("Helloeee"))
+	//	System.out.println(s3.toString());
+		if(s3.containE("Helloeee"))
 			System.out.println("true");
 		else
-			System.out.println("false");*/
+			System.out.println("false");
 	}
 }
