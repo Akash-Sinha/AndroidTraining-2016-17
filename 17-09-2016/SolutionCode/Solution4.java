4d31c4d1d63fc5520a1ecbb57ebb04a3aa68aa49
Akash-Sinha <akashsinha.369@gmail.com>
2016-10-03 19:36:39 +0530

    Delete Solution4.java

--- 17-09-2016/SolutionCode/Solution4.java
+++
@@ -1,9 +0,0 @@
-public class Solution4 {
-	public String embedWord(String s1, String s2) {
-		StringBuilder sb = new StringBuilder(s1);
-		
-		sb.insert(s1.length()/2, s2);
-		
-		return sb.toString();
-	}
-}
