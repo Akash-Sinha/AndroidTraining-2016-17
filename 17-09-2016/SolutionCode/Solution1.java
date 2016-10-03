408d732a7c149cc021392b91a03af256368b7883
Akash-Sinha <akashsinha.369@gmail.com>
2016-10-03 19:36:05 +0530

    Delete Solution1.java

--- 17-09-2016/SolutionCode/Solution1.java
+++
@@ -1,9 +0,0 @@
-public class Solution1 {
-	
-	public String missingChar(String s, int n) {
-		String str = new String("");
-		str = s.substring(0,n); 
-		str += s.substring(n+1);
-		return str;	
-	}
-}
